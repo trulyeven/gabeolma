2b969a2e783d7faae91d6ce719b5d60e28deffdc
trulyeven <trulyeven@gmail.com>
2023-09-04 22:02:06 +0900

    firebase 데이터베이스 연동 #1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { Link } from 'react-router-dom';
 import './App.css';
 import Router from './router/Router';
+// Required for side-effects
+import "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from 'react';
+import { db } from "./firebase-config";
 
 function App() {
+
+  
+  useEffect(async () => {
+    // db 뒤에 "techInfo"는 정보를 가져올 컬렉션 이름이다.
+      const query = await getDocs(collection(db, "users")); 
+      query.forEach((doc) => {
+        console.log(doc.id, doc.data())
+      });
+    }, [])
+
+  
   return (<Router />,
     <div className="App">
       <Link to='/'>홈</Link>
-      
+
       <hr />
       APP
       
---
+++ src/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANusLrt3_AqHSLcMYOq1q-PcCYrYz9t2w",
+  authDomain: "gabeolma.firebaseapp.com",
+  databaseURL: "https://gabeolma-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gabeolma",
+  storageBucket: "gabeolma.appspot.com",
+  messagingSenderId: "736809144494",
+  appId: "1:736809144494:web:3a1a52d7c9818f147a5d3f",
+  measurementId: "G-5RPZXMZET0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// 사용하기 위해 export
+export const db = getFirestore();
