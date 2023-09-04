dbc6ea8ee6f5b7fa3c4f896136102f868a2f1c71
trulyeven <trulyeven@gmail.com>
2023-09-04 23:09:07 +0900

    github actions CI/CD #8

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // 사용하기 위해 export
-const db = getFirestore();
-export { db };
+export const db = getFirestore(app);
